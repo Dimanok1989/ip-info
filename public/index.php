8be3b3eab95ff3a83bfb5163e72ffd7324a27844
Dimanok1989 <kolgaev@gmail.com>
2022-02-28 13:18:09 +0300

    Главный скрипт

--- public/index.php
+++ public/index.php
@@ -4,16 +4,17 @@ require __DIR__ . "/../vendor/autoload.php";
 
 try {
     $ip = new \Kolgaev\IpInfo\Ip(__DIR__ . "/../config/env.php");
+    $check = $ip->check();
 
-    if (!empty($ip['block'])) {
-        if ($ip['block'] == true) {
+    if (!empty($check['block'])) {
+        if ($check['block'] == true) {
             http_response_code(500);
             exit;
         }
     }
 
     if (getenv("KOLGAEV_STATS_DEBUG")) {
-        echo json_encode($ip->check());
+        echo json_encode($check);
     }
 } catch (\Exception $e) {
     if (getenv("KOLGAEV_STATS_DEBUG")) {
